2de147d818d808b2aa944c969d14d30254affe99
hrmasss <hojayfarahman@gmail.com>
2024-02-13 01:40:27 +0600

    Create header and footer

---
+++ src/data/nav-links.ts
@@ -0,0 +1,11 @@
+type NavLink = {
+    text: string;
+    href: string;
+};
+
+export const navLinks: NavLink[] = [
+    { text: "work", href: "/#work" },
+    { text: "writing", href: "/#writing" },
+    { text: "about", href: "/#about" },
+    { text: "contact", href: "/#contact" },
+];
