8f8d46bcf676821839fb8dc58b1b93331fe5bb31
hrmasss <hojayfarahman@gmail.com>
2024-02-13 11:18:34 +0600

    Remove react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import { remarkReadingTime } from "@/utils/remark-reading-time.mjs";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 import mdx from "@astrojs/mdx";
 
@@ -11,7 +10,6 @@ export default defineConfig({
         remarkPlugins: [remarkReadingTime],
     },
     integrations: [
-        react(),
         tailwind({
             applyBaseStyles: false,
         }),
